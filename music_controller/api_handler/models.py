3fa2cf7bb52e7515096142b7b7a0a3e596df23a9
kubosis <kubasukdol@gmail.com>
2024-11-15 18:54:04 +0100

    [BE: feature] basic api view

--- music_controller/api_handler/models.py
+++ music_controller/api_handler/models.py
@@ -4,13 +4,14 @@ import string
 import random
 
 def generate_uid_code():
-    length = 8
-    while True:
-        code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Room.objects.filter(id_code=code).count() == 0:
-            # the code is unique
-            break 
-    return code
+        length = 8
+        while True:
+            code = ''.join(random.choices(string.ascii_uppercase, k=length))
+            if Room.objects.filter(id_code=code).count() == 0:
+                # the code is unique
+                break 
+        return code
+
 
 # Create your models here.
 class Room(models.Model):
